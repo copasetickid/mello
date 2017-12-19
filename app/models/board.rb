86b11b1aa2ad09b36e26a40d5243e5a46d6e08c7
Rushaine McBean <rmcbean@businessinsider.com>
2017-12-19 17:53:22 -0500

    adds members associations to board model

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,6 @@
 class Board < ApplicationRecord
+  has_many :board_members
+  has_many :members, through: :board_members
+
   validates :name, presence: true
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Board, type: :model do
   context "Validations" do
     it { should validate_presence_of(:name) }
   end
+
+  context "Associations" do
+    it { should have_many(:board_members) }
+    it { should have_many(:members).through(:board_members)}
+  end
 end
