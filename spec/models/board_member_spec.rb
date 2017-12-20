4ddacd8fe77164536e8be5f17312b8a94eff7898
Rushaine McBean <rmcbean@businessinsider.com>
2017-12-20 15:20:46 -0500

    Model spec cleanup

--- spec/models/board_member_spec.rb
+++ spec/models/board_member_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe BoardMember, type: :model do
   describe "Associations" do
     it { should belong_to(:board) }
-    it { should belong_to(:member) }
+    it { should belong_to(:member).class_name('User') }
   end
 end
