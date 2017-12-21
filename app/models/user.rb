3f9f4c0737a5c0154d9d713038133a5fec875d33
Rushaine McBean <rmcbean@businessinsider.com>
2017-12-21 16:58:08 -0500

    Adds a card association through the card assignee model for a user to see which cards they've been assigned to

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :boards, foreign_key: "creator_id"
   has_many :board_members, foreign_key: "member_id"
   has_many :memberships, through: :board_members, source: :board
+  has_many :card_assignees
+  has_many :cards, through: :card_assignees
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe User, type: :model do
     it { should have_many(:board_members).with_foreign_key('member_id') }
     it { should have_many(:memberships).through(:board_members).source(:board) }
     it { should have_many(:boards).with_foreign_key('creator_id') }
+    it { should have_many(:card_assignees) }
+    it { should have_many(:cards).through(:card_assignees) }
   end
 end
