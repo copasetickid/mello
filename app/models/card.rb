b89617380042c633a76af14ccff71e5ca271a208
Rushaine McBean <rmcbean@businessinsider.com>
2017-12-21 16:56:33 -0500

    Adds the card assignees assocation to the card model for access to assignees

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,7 @@
 class Card < ApplicationRecord
   belongs_to :list
+  has_many :card_assignees
+  has_many :assignees, through: :card_assignees, source: :user
 
   validates :title, presence: true
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Card, type: :model do
   describe 'Assocations' do
     it { should belong_to(:list) }
+    it { should have_many(:card_assignees) }
+    it { should have_many(:assignees).through(:card_assignees).source(:user) }
   end
 
   describe 'Validations' do
